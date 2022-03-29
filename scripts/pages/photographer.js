2c0cf10908d1ba86e049c8c0113a70a473e85501
sergyth <73670477+sergyth@users.noreply.github.com>
2022-03-29 07:57:14 +0200

    positionement de la likebox

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -12,91 +12,104 @@ fetch("../data/photographers.json")
   .then((res) => res.json())
   .then((data) => {
     let photographer = data.photographers.find((item) => item.id == id);
-    console.log(data);
     let medias = data.media.filter((item) => item.photographerId == id);
-    console.log(medias);
-    displayProfile(photographer)
+    displayProfile(photographer);
     displayDropdownMenu();
-    const gallery = document.createElement("section");
-    gallery.classList.add("gallery");
-    document.querySelector("#main").appendChild(gallery);
-    let totalLikes = 0;
-    medias.forEach((media) => {
-        displayMedia(media, photographer);
-      }
-    );
-    const likesBox = document.createElement("div");
-    likesBox.classList.add("totalLikes");
-    likesBox.innerHTML = displayLikes(photographer, totalLikes);
-    document.querySelector("#main").appendChild(likesBox);
+    displayMedia(medias, photographer);
+    displayTotal(photographer, medias);
   });
 
-function displayProfile(photographer){
-    document.querySelector(".photograph-header").innerHTML =
+function displayProfile(photographer) {
+  document.querySelector(".photograph-header").innerHTML =
     renderProfile(photographer);
-  }
+}
 
 function renderProfile(photographer) {
   return `
-          <div>
-            <h1>${photographer.name}</h1>   
-            <h2>${photographer.city},  ${photographer.country}</h2>
-            <p>${photographer.tagline}</p>
-          </div>
-          <div>
-            <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
-          </div>
-          <div>
-            <img src='../assets/photographers/${photographer.portrait}' class='photographer-profile_img'/>
-          </div>
-          
-       `;
+    <div>
+      <h1>${photographer.name}</h1>   
+      <h2>${photographer.city},  ${photographer.country}</h2>
+      <p>${photographer.tagline}</p>
+    </div>
+    <div>
+      <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+    </div>
+    <div>
+      <img src='../assets/photographers/${photographer.portrait}' class='photographer-profile_img'/>
+    </div>`;
 }
 
-function displayDropdownMenu(){
-    document.querySelector("#main").innerHTML += renderDropdownMenu();
+function displayDropdownMenu() {
+  document.querySelector("#main").innerHTML += renderDropdownMenu();
 }
 
 function renderDropdownMenu() {
   return `
-        <div class="dropdown-menu">
-          <label for ="dropdown-menu_block">Trier par</label>
-          <select id='dropdown-menu_block'>
-            <i class="fa-solid fa-circle-chevron-up fa-3x"></i> 
-            <option value ='popularité' class="dropdown-menu_block-item">Popularité</option>
-            <option value ='Date' class="dropdown-menu_block-item">Date</option>
-            <option value ='Titre' class="dropdown-menu_block-item">Titre</option>
-          </select>
-        </div>
-      `;
+    <div class="dropdown-menu">
+      <label for ="dropdown-menu_block">Trier par</label>
+      <select id='dropdown-menu_block'>
+        <i class="fa-solid fa-circle-chevron-up fa-3x"></i> 
+        <option value ='popularité' class="dropdown-menu_block-item">Popularité</option>
+        <option value ='Date' class="dropdown-menu_block-item">Date</option>
+        <option value ='Titre' class="dropdown-menu_block-item">Titre</option>
+      </select>
+    </div>`;
 }
 
-function displayMedia(media, photographer){
-    totalLikes += media.likes;
-    document.querySelector(".gallery").innerHTML += renderMedia(media, photographer);
+function displayMedia(medias, photographer) {
+  const gallery = document.createElement("article");
+  gallery.classList.add("gallery");
+  document.querySelector("#main").appendChild(gallery);
+  medias.forEach((media) => {
+    document.querySelector(".gallery").innerHTML += renderMedia( media, photographer);
+  });
+
 }
 
 function renderMedia(media, photographer) {
-    if(media.image){
-    return`
-        <article class='gallery-block-item'>
-          <img src='../assets/images/${photographer.name}/${media.image}' class='gallery-img'/>
-          <div class="gallery-item-title">
+  if (media.image) {
+    return `
+      <article class='gallery-block-item'>
+        <img src='../assets/images/${photographer.name}/${media.image}' class='media' />
+        <div class="gallery-item-title">
             <p class='gallery_item-title'>${media.title}</p>
             <p  class='gallery_item-likes'>${media.likes}
-              <i class="fa-solid fa-heart "></i>
+            <i class="fa-solid fa-heart "></i>
             </p>
-         </div>
-        </article>`;
-    } 
-    
-}
-function displayLikes(photographer, totalLikes) {
-  return `
-        <div>
-          <span>${totalLikes}</span>
-          <span>${photographer.price}/Jour</span>  
         </div>
+      </article>`;
+  }
 
-      `;
+  if (media.video) {
+    return `
+      <article class='gallery-block-item'>
+        <video controls class='media'>
+            <source src="../assets/images/${photographer.name}/${media.video}" type="video/mp4" >   
+        </video>
+        <div class="gallery-item-title">
+            <p class='gallery_item-title'>${media.title}</p>
+            <p  class='gallery_item-likes'>${media.likes}
+            <i class="fa-solid fa-heart "></i>
+            </p>
+        </div>
+      </article>`;
+  }
+}
+function displayTotal(photographer, medias){
+  let totalLikes = 0;
+  medias.forEach(media => {
+    totalLikes += media.likes;
+  })
+  const likesBox = document.createElement("div");
+  likesBox.classList.add("totalLikes");
+  likesBox.innerHTML = renderTotal(photographer, totalLikes);
+  document.querySelector("#main").appendChild(likesBox);
+}
+function renderTotal(photographer, totalLikes) {
+  return `
+    <div>
+      <span>${totalLikes}</span>
+      <i class="fa-solid fa-heart "></i>
+      <span class="totalLikes-price">${photographer.price}/Jour</span>  
+    </div>`;
 }
